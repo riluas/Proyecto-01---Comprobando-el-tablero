a4513886c727d87c12482108410cfa2229ad66e6
ALUMNADO\riluas <riluas@floridauniversitaria.es>
2018-10-17 12:58:19 +0200

    Proyecto 01 Ricardo Lucas

---
+++ codigo.js
@@ -0,0 +1,14 @@
+var columna=Number(prompt("Numero de columna"))
+var fila=Number(prompt("Numero de fila"))
+
+if ((columna>=1) && (columna<=6) && (fila>=1) && (fila<=6)) {
+console.log("Bien");
+}
+
+if ((columna<1) || (columna>6)){
+  alert(`Error en columna ${columna}`)
+}
+
+if ((fila<1) || (fila>6)){
+  alert(`Error en fila ${fila}`)
+}
